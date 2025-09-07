1d5977f11e8b09a42088af4a8af07389e9ab1ae6
W.A.R.J <frivandres2011@hotmail.com>
2025-09-06 22:12:10 -0500

    WR: Update -> 15

--- projects/15_calendar_and_dataentry/main.py
+++ projects/15_calendar_and_dataentry/main.py
@@ -19,7 +19,7 @@ def get_date(date):
 calendar.bind("<<CalendarSelected>>", lambda _: get_date(calendar.get_date()))
 
 # dateentry
-date_entry = DateEntry(root, selectmode="day", locale="es_ES", year=2025, month=9, day=6)
+date_entry = DateEntry(root, selectmode="day", locale="es_ES", year=2025, month=9, day=6, date_pattern="dd-mm-y")
 date_entry.pack(expand=1, fill="both")
 
 date_entry.bind("<<DateEntrySelected>>", lambda _: get_date(date_entry.get_date().strftime("%d-%m-%Y")))
